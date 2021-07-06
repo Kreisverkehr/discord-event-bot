80569026c41d580c26ca45ff9bb3ca418d5be71e
Kreisverkehr <mm.rueter@gmail.com>
2021-07-06 19:07:51 +0200

    disable prefix for private messages

--- DiscordEventBot.Service/Services/CommandHandlingService.cs
+++ DiscordEventBot.Service/Services/CommandHandlingService.cs
@@ -68,7 +68,7 @@ namespace DiscordEventBot.Service.Services
             var argPos = 0;
             // Perform prefix check. You may want to replace this with (!message.HasCharPrefix('!',
             // ref argPos)) for a more traditional command format like !help.
-            if (!message.HasMentionPrefix(_discord.CurrentUser, ref argPos)) return;
+            if (!message.HasMentionPrefix(_discord.CurrentUser, ref argPos) && !(rawMessage.Channel is IDMChannel)) return;
 
             var context = new SocketCommandContext(_discord, message);
             // Perform the execution of the command. In this method, the command service will
