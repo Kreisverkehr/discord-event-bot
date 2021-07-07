7c8facd512d8510525d61e15a3a031b2873fd624
Kreisverkehr <mm.rueter@gmail.com>
2021-07-07 13:48:38 +0200

    created message framework

---
+++ DiscordEventBot.Common/Messages/MessageBase.cs
@@ -0,0 +1,52 @@
+﻿using Discord;
+
+namespace DiscordEventBot.Common.Messages
+{
+    public abstract class MessageBase
+    {
+        #region Private Fields
+
+        private bool isBuilt = false;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        public Embed Embed { get; private set; }
+        public string MessageText { get; private set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public static implicit operator Embed(MessageBase msg)
+        {
+            msg.Build();
+            return msg.Embed;
+        }
+
+        public static implicit operator string(MessageBase msg)
+        {
+            msg.Build();
+            return msg.MessageText;
+        }
+
+        public void Build()
+        {
+            if (isBuilt) return;
+            MessageText = BuildMessageText();
+            Embed = BuildEmbed();
+            isBuilt = true;
+        }
+
+        #endregion Public Methods
+
+        #region Protected Methods
+
+        protected virtual Embed BuildEmbed() => null;
+
+        protected virtual string BuildMessageText() => string.Empty;
+
+        #endregion Protected Methods
+    }
+}
