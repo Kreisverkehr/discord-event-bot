3200ba45e91289f04090b8afbda64fe580299a29
Kreisverkehr <mm.rueter@gmail.com>
2021-07-26 22:06:10 +0200

    add downloadable ics file to event detail messages

---
+++ DiscordEventBot.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordEventBot.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToFilename(this string text, string extension)
+        {
+            StringBuilder sb = new StringBuilder(text);
+            foreach (var c in Path.GetInvalidFileNameChars())
+                sb.Replace(c.ToString(), null);
+            sb.Append('.');
+            sb.Append(extension);
+            return sb.ToString();
+        }
+    }
+}
