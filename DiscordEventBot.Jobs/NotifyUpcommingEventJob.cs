5a25180b42c57449c9eaff9ec9af111d8024379f
Kreisverkehr <mm.rueter@gmail.com>
2021-08-02 17:22:42 +0200

    test DI with jobs

--- DiscordEventBot.Jobs/NotifyUpcommingEventJob.cs
+++ DiscordEventBot.Jobs/NotifyUpcommingEventJob.cs
@@ -1,4 +1,6 @@
-﻿using Quartz;
+﻿using Discord.WebSocket;
+using DiscordEventBot.Model;
+using Quartz;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,19 @@ namespace DiscordEventBot.Jobs
 {
     public class NotifyUpcommingEventJob : IJob
     {
+        private readonly EventBotContext _dbContext;
+        private readonly DiscordSocketClient _client;
+
+        public NotifyUpcommingEventJob(EventBotContext dbContext, DiscordSocketClient client)
+        {
+            _dbContext = dbContext;
+            _client = client;
+        }
         public async Task Execute(IJobExecutionContext context)
         {
             Console.WriteLine("Hello World from Job.");
+            foreach (var usr in _dbContext.Users)
+                Console.WriteLine(_client.GetUser(usr.UserId).Username + "#" + _client.GetUser(usr.UserId).Discriminator);
         }
     }
 }
