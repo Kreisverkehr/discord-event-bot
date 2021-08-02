b50cc6d2c0d2be62323d19b973d865b7cac8086f
Kreisverkehr <mm.rueter@gmail.com>
2021-08-02 17:08:01 +0200

    add jobs proj to git

---
+++ DiscordEventBot.Jobs/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordEventBot.Jobs.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddJobs(this IServiceCollection services) => services
+            .AddQuartz(config =>
+            {
+                config.SchedulerId = "discord-event-bot-scheduler";
+                config.SchedulerName = "Discord Event Bot Scheduler";
+                config.UseMicrosoftDependencyInjectionJobFactory();
+                config.UseSimpleTypeLoader();
+                config.UseInMemoryStore();
+                config.UseDefaultThreadPool(tp =>
+                {
+                    tp.MaxConcurrency = 10;
+                });
+                config.ScheduleJob<NotifyUpcommingEventJob>(trigger => trigger
+                    .WithIdentity("every minute")
+                    .WithDailyTimeIntervalSchedule(x => x.WithIntervalInMinutes(1))
+                );
+                
+            })
+            .AddQuartzHostedService()
+            ;
+    }
+}
---
+++ DiscordEventBot.Jobs/NotifyUpcommingEventJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordEventBot.Jobs
+{
+    public class NotifyUpcommingEventJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Hello World from Job.");
+        }
+    }
+}
