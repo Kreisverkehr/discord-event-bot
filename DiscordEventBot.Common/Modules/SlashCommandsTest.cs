2f1051afc043497280db70e1124e3e1f5bcf9e64
Kreisverkehr <mm.rueter@gmail.com>
2021-12-22 13:56:20 +0100

    implement basic interaction structure

---
+++ DiscordEventBot.Common/Modules/SlashCommandsTest.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordEventBot.Common.Modules
+{
+    public class SlashCommandsTest : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("test-method", "this is a testmethod")]
+        public async Task TestMethod()
+        {
+            //await ReplyAsync("Test successful");
+            await this.RespondAsync("Test successful");
+        }
+    }
+}
